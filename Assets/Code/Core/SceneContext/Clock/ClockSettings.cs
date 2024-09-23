fc23b8a27896470a26e7071bd4b8c0d8acd9761e
Ivan Sergeevich <iwan1234554321@gmail.com>
2024-09-23 14:38:51 +0500

    added animation params for clock

--- Assets/Code/Core/SceneContext/Clock/ClockSettings.cs
+++ Assets/Code/Core/SceneContext/Clock/ClockSettings.cs
@@ -6,8 +6,14 @@ public class ClockSettings : ScriptableObject
     [SerializeField] private int hourOfDay = 24;
     [SerializeField] private int minutesOfHour = 60;
     [SerializeField] private int secondsOfMinutes = 60;
+    [Space]
+    [SerializeField] private float          animationTime = 2.0f;
+    [SerializeField] private AnimationCurve animationTweenCurve;
 
     public int HourOfDay        => hourOfDay;
     public int MinutesOfHour    => minutesOfHour;
     public int SecondsOfMinutes => secondsOfMinutes;
+
+    public float          AnimationTime       => animationTime;
+    public AnimationCurve AnimationTweenCurve => animationTweenCurve;
 }
